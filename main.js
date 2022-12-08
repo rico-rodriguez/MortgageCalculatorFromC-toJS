524065f426f8178cc4a39be59b50e2895a410c79
Rico Rodriguez <97664519+rico-rodriguez@users.noreply.github.com>
2022-12-09 07:04:57 +0900

    removed calculator file

--- main.js
+++ main.js
@@ -18,15 +18,6 @@ const formatter = new Intl.NumberFormat('en-US', {
   currency: 'USD',
 });
 function calculate() {
-  const result = calculatePayment(
-    purchasePrice,
-    marketValue,
-    downPayment,
-    yearlyIncome,
-    interestRate,
-    loanTerm,
-    hoaFee
-  );
   modal.style.display = 'block';
   monthlyPayment.innerHTML = `<br>Your monthly income is: 
   ${formatter.format(yearlyIncome.value)}
@@ -60,4 +51,63 @@ function showHoaFeeBox() {
   } else {
     console.log(hoaSelector.checked);
   }
+}
+
+function loanInsuranceCalculator(totalLoanAmount, buyersEquityPercentage) {
+  if (buyersEquityPercentage >= 10) {
+    return 0;
+  } else {
+    // This is additional monthly payment incurred for having loan insurance
+    return (0.01 * totalLoanAmount) / 12;
+  }
+}
+function calculatePayment(
+  purchasePrice,
+  marketValue,
+  downPayment,
+  yearlyIncome,
+  interestRate,
+  loanTerm,
+  hoaFee
+) {
+  var output = JSON.parse('{"initialLoanAmount":0}');
+  var initialLoanAmount = purchasePrice - downPayment;
+  output.initialLoanAmount = initialLoanAmount;
+  const ORIGINATION_FEE = 0.01;
+  var originationFeeAmount = initialLoanAmount * ORIGINATION_FEE;
+  output.originationFeeAmount = originationFeeAmount;
+  const CLOSING_COSTS = 2500;
+  var totalLoanAmount =
+    initialLoanAmount + originationFeeAmount + CLOSING_COSTS;
+  var equityValue = marketValue - totalLoanAmount;
+  var buyersEquityPercentage = (equityValue / marketValue) * 100;
+  var monthlyInsuredLoan = loanInsuranceCalculator(
+    totalLoanAmount,
+    buyersEquityPercentage
+  );
+  // HOA
+  var yearlyHOAFees = hoaFee;
+  var monthlyHOAFees = yearlyHOAFees / 12;
+  // Property Tax
+  var propTaxRate = 0.0125;
+  var yearlyPropTax = marketValue * propTaxRate;
+  var monthlyPropTax = yearlyPropTax / 12;
+  // Get yearlyInsurance
+  var insuranceRate = 0.0075;
+  var yearlyInsurance = marketValue * insuranceRate;
+  var monthlyHomeOwnersInsurance = yearlyInsurance / 12;
+  var loanPaymentAmount =
+    totalLoanAmount *
+    (interestRate / 12) *
+    (Math.pow(1 + interestRate / 12, 12 * loanTerm) /
+      (Math.pow(1 + interestRate / 12, 12 * loanTerm) - 1));
+  var totalMonthlyPayment =
+    loanPaymentAmount +
+    monthlyHomeOwnersInsurance +
+    monthlyHOAFees +
+    monthlyPropTax +
+    monthlyInsuredLoan;
+  output.totalMonthlyPayment = totalMonthlyPayment.toFixed(2);
+  console.log(output);
+  return output;
 }
